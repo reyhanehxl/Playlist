74c2b92241defa55fcbd8731f93b772a8bb9f61c
reyhanehxl <reyhaneh.amanollahii@gmail.com>
2025-04-18 16:00:59 +0330

    add main to test

--- Main.java
+++ Main.java
@@ -1,5 +1,33 @@
+import Playlist.InvalidOperationException;
+import Playlist.Music;
+import Playlist.RegularBehavior;
+import Playlist.User;
 
-public class Main {
-    public static void main(String[] args) {
- }
-}
+
+
+        public class Main {
+            public static void main(String[] args) {
+                try {
+                    User user1 = new User("reyhane", "passWord1", new RegularBehavior());
+                    System.out.println("Created user: " + user1.getUsername());
+
+                    Music song1 = new Music("Symphony No.5", user1);
+                    Music song2 = new Music("Moonlight Sonata", user1);
+                    user1.playMusic(song1);
+                    user1.playMusic(song2);
+                    user1.playMusic(song1);
+                    user1.playMusic(song2);
+                    user1.playMusic(song1);
+                    user1.buyPremium(user1, 2);
+                    System.out.println("Upgraded to premium");
+
+                    user1.playMusic(song2);
+
+                    user1.createPlaylist("Sisi's Hits", user1);
+                    System.out.println("Playlist created.");
+
+                } catch (InvalidOperationException e) {
+                    System.out.println("Operation failed: " + e.getMessage());
+                }
+            }
+        }
