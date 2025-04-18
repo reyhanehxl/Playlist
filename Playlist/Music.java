99a43d2ee10072664575e13ae960bd8c11b4ffc4
reyhanehxl <reyhaneh.amanollahii@gmail.com>
2025-04-18 12:16:33 +0330

    Set Music class

--- Playlist/Music.java
+++ Playlist/Music.java
@@ -1,4 +1,37 @@
 package Playlist;
 
+import java.util.ArrayList;
+
 public class Music {
+    final String title;
+    final User singer;
+    private int numberOfStream;
+    private static ArrayList<Music> allMusics = new ArrayList<>();
+
+    public Music(String title, User singer){
+        this.title = title;
+        this.singer = singer;
+        this.numberOfStream = 0;
+        allMusics.add(this);
+    }
+    private void play(){
+        System.out.println("Music: " + this.title + "\nSinger: " + this.singer + "\nPlayed: " + this.numberOfStream);
+        this.numberOfStream++;
+    }
+    private Music search(String name){
+        for(Music music : allMusics){
+            if(music.title.equalsIgnoreCase(name)){
+                return music;
+            }
+        }
+        return null;
+    }
+    private Music search(String name, String singer){
+        for(Music music : allMusics){
+            if(music.title.equalsIgnoreCase(name) && music.singer.getUsername().equalsIgnoreCase(singer)){
+                return music;
+            }
+        }
+        return null;
+    }
 }
