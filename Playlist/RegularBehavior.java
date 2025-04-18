9a7a00d4a4de6744196be685c70747d2d0130265
reyhanehxl <reyhaneh.amanollahii@gmail.com>
2025-04-18 10:21:02 +0330

    Set RegularBehavior clas

--- Playlist/RegularBehavior.java
+++ Playlist/RegularBehavior.java
@@ -1,4 +1,25 @@
 package Playlist;
 
-public class RegularBehavior {
+public class RegularBehavior implements UserBehavior{
+
+    private int playingLimit = 5;
+
+    @Override
+    public void createPlaylist(String Title, User Owner) {
+        throw new InvalidOperationException("Upgrade to premium to create playlists.");
+    }
+
+    @Override
+    public void playMusic(Music music) {
+        if(playingLimit <= 0){
+           throw new InvalidOperationException("Limit reached.Upgrade to get more!");
+        }
+        playingLimit--;
+            // play the music
+    }
+
+    @Override
+    public void buyPremium(User owner, int month) {
+
+    }
 }
